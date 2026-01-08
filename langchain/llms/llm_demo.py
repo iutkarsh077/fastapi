4ad0c1446b6a616564cd2f8f592501b43fd0733a
utkarsh <utkarshsingh132003@gmail.com>
2026-01-08 19:43:34 +0530

    chat models

---
+++ langchain/llms/llm_demo.py
@@ -0,0 +1,8 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", max_tokens=10)
+response = model.invoke("Who is Virat Kohli answer in 20 words only")
+print(response)
