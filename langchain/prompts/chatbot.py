5e4b1defe9ee64b62a18fb6843ea497e97bcae84
utkarsh <utkarshsingh132003@gmail.com>
2026-01-10 12:56:24 +0530

    messages types

--- langchain/prompts/chatbot.py
+++ langchain/prompts/chatbot.py
@@ -1,19 +1,22 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", max_tokens=100)
 
-chatHistory = []
+# chatHistory = []
+
+messages = [
+    SystemMessage("You are a assistant")
+]
 
 while True:
     user_input = input("Enter your question: ")
-    answers = { "role": "user", "content": user_input }
     if user_input == "exit":
         break
-    chatHistory.append(answers)
-    response = model.invoke(chatHistory)
-    answers2 = { "role": "assistant", "content": response.content }
-    chatHistory.append(answers2)
+    messages.append(HumanMessage(user_input))
+    response = model.invoke(messages)
+    messages.append(AIMessage(response.content))
     print(response.content)
