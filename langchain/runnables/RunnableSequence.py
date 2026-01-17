2cadcfae7246d922eb293334458339b374aacaf0
utkarsh <utkarshsingh132003@gmail.com>
2026-01-17 20:42:47 +0530

    runnable sequence

---
+++ langchain/runnables/RunnableSequence.py
@@ -0,0 +1,23 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", max_tokens=500)
+
+template = PromptTemplate(
+    template="Write a joke on {topic}",
+    input_variables=["topic"]
+)
+
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(template, model, parser)
+
+result = chain.invoke({ "topic": "AI" })
+
+print(result)
