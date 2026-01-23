803e9b2286d2107d454930a53afff1fd206c6bc5
utkarsh <utkarshsingh132003@gmail.com>
2026-01-23 11:15:34 +0530

    text splitters

---
+++ langchain/text-splitters/demo1.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+
+# loader = TextLoader("./poem.txt", encoding="utf-8")
+
+# data = loader.load()
+
+myfile = ""
+
+with open("./poem.txt", "r") as f:
+    myfile = f.read()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=10)
+texts = text_splitter.split_text(myfile.strip())
+
+print("\n")
+print("\n")
+print("\n")
+
+print(texts)
